e0a78d5c6a094db69b82d2ff4ccb80ba28bbf9cb
B-chris-dev <christophe.bouteille.91@gmail.com>
2025-10-03 14:07:53 +0200

    login

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/login", indexRouter);
+app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/catways", catwaysRouter);
 // catch 404 and forward to error handler
